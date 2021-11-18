cf270b1b4e470fc7ad8e5c8bf27a624d151fbe1b
rivernine <rivernine@github.com>
2021-11-18 11:11:02 +0900

    add: checkMFI

--- src/main/java/com/rivernine/cryptoGenerator/schedule/DayTradingJobScheduler.java
+++ src/main/java/com/rivernine/cryptoGenerator/schedule/DayTradingJobScheduler.java
@@ -1,5 +1,6 @@
 package com.rivernine.cryptoGenerator.schedule;
 
+import java.util.Arrays;
 import java.util.List;
 
 import com.google.gson.JsonObject;
@@ -13,7 +14,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import ch.qos.logback.core.joran.conditional.ElseAction;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -57,22 +57,51 @@ public class DayTradingJobScheduler {
     return (pos <= 0.1) ? true : false;
   }
 
-  // public Double calculateMFI(JsonObject[] objs){
-  //   for(JsonObject obj: objs) {
+  public Boolean checkMFI(JsonObject[] objs){
+    Double[] tpList = new Double[11];
+    Double[] volList = new Double[11];
 
-  //   }
-  // }
+    for(int i = 0; i < objs.length; i++){
+      Double tp = (objs[i].get("high_price").getAsDouble() + objs[i].get("low_price").getAsDouble() + objs[i].get("trade_price").getAsDouble()) / 3;
+      Double vol = objs[i].get("candle_acc_trade_volume").getAsDouble();
+      tpList[i] = tp;
+      volList[i] = vol;
+    }
 
-  // public Boolean checkMFI(JsonObject[] objs){
+    Double positiveRMF = 0.0;
+    Double negativeRMF = 0.0;
+    for(int i = 0; i < objs.length - 2; i++){
+      Double tp = tpList[i];
+      Double befTp = tpList[i + 1];
+      if(tp > befTp)
+        positiveRMF += tp * volList[i];
+      else
+        negativeRMF += tp * volList[i];
+    }
+    Double MFI = positiveRMF / (positiveRMF + negativeRMF) * 100;
 
-  // }
+    positiveRMF = 0.0;
+    negativeRMF = 0.0;
+    for(int i = 1; i < objs.length - 1; i++){
+      Double tp = tpList[i];
+      Double befTp = tpList[i + 1];
+      if(tp > befTp)
+        positiveRMF += tp * volList[i];
+      else
+        negativeRMF += tp * volList[i];
+    }
+    Double befMFI = positiveRMF / (positiveRMF + negativeRMF) * 100;
+
+    return (MFI < 20 && MFI - befMFI > 0) ? true : false;
+  }
 
   @Scheduled(fixedDelay = 1000)
   public void runGetMultipleCandlesJob() {
     for(String market: markets) {
       JsonObject[] result = getCandleJobConfiguration.getCandlesJob(market, "5", "20");
       log.info("{}", result[0]);
-      System.out.println(checkBollinger(result));
+      log.info("boll: {}", checkBollinger(result));
+      log.info("mfi: {}", checkMFI(Arrays.copyOfRange(result, 0, 11)));
     }
   }
 }
