0e2c4383deb672c6678b20b78e3dfbe7606ff9ee
rivernine <rivernine@github.com>
2021-11-18 10:28:29 +0900

    add: checkBollinger

--- src/main/java/com/rivernine/cryptoGenerator/schedule/DayTradingJobScheduler.java
+++ src/main/java/com/rivernine/cryptoGenerator/schedule/DayTradingJobScheduler.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import ch.qos.logback.core.joran.conditional.ElseAction;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -33,32 +34,45 @@ public class DayTradingJobScheduler {
   private final AnalysisForScaleTradingJobConfiguration analysisForScaleTradingJobConfiguration;
   private final OrdersJobConfiguration ordersJobConfiguration;
 
+  public Boolean checkBollinger(JsonObject[] objs) {
+    Double sum = 0.0;
+    Double avg = 0.0;
+    Double var = 0.0;
+    Double std = 0.0;
+
+    for (JsonObject obj: objs)
+      sum += obj.get("trade_price").getAsDouble();
+    avg = sum / objs.length;
+    for (JsonObject obj: objs)
+      var += Math.pow(obj.get("trade_price").getAsDouble() - avg, 2);
+    var = var / (objs.length);
+    std = Math.sqrt(var);
+
+    Double lo = avg - std * 2;
+    Double me = avg;
+    Double hi = avg + std * 2;
+    Double pos = (objs[0].get("trade_price").getAsDouble() - lo) / (hi - lo);
+    log.info("lo: " + String.format("%.0f", lo) + " me: " + String.format("%.0f", me) + " hi: " + String.format("%.0f", hi) + " pos: " + String.format("%.2f", pos));
+    
+    return (pos <= 0.1) ? true : false;
+  }
+
+  // public Double calculateMFI(JsonObject[] objs){
+  //   for(JsonObject obj: objs) {
+
+  //   }
+  // }
+
+  // public Boolean checkMFI(JsonObject[] objs){
+
+  // }
+
   @Scheduled(fixedDelay = 1000)
   public void runGetMultipleCandlesJob() {
     for(String market: markets) {
-      JsonObject[] result = getCandleJobConfiguration.getCandlesJob(market, "30", "5");
-      Double sum = 0.0;
-      Double avg = 0.0;
-      Double var = 0.0;
-      Double std = 0.0;
-      // for (int i = 0; i < result.length; i++){
-
-      // }
-      for (JsonObject obj: result)
-        sum += obj.get("trade_price").getAsDouble();
-      avg = sum / result.length;
-      for (JsonObject obj: result){
-        var += Math.pow(obj.get("trade_price").getAsDouble() - avg, 2);
-      }
-      var = var / (result.length);
-      std = Math.sqrt(var);
-
-      Double lo = avg - std * 2;
-      Double me = avg;
-      Double hi = avg + std * 2;
-      Double pos = (result[0].get("trade_price").getAsDouble() - lo) / (hi - lo);
-      log.info("lo: " + String.format("%.0f", lo) + " me: " + String.format("%.0f", me) + " hi: " + String.format("%.0f", hi) + " pos: " + String.format("%.2f", pos));
-      // log.info(sum);
+      JsonObject[] result = getCandleJobConfiguration.getCandlesJob(market, "5", "20");
+      log.info("{}", result[0]);
+      System.out.println(checkBollinger(result));
     }
   }
 }
